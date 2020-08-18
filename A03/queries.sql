11d859e820fcd504a622b3c40661ed9c8827a389
NearLawiet <sooraj1999vs@gmail.com>
2020-08-19 02:18:41 +0530

    formatted queries

--- A03/queries.sql
+++ A03/queries.sql
@@ -1,5 +1,6 @@
 use A03;
 
+# 02
 SELECT * FROM customer 
 WHERE 
 	city='Mumbai'
@@ -10,33 +11,39 @@ WHERE
 	or
 	cust_fname like 'H%';
 
+# 03
 SELECT DISTINCT city FROM customer;
 
+# 04
 UPDATE book SET
 	unit_price = unit_price*1.05
 	WHERE pub_year=2019;
-	
+
+# 05	
 DELETE FROM customer WHERE city = 'Pune';
 
-
-select * from book 
-WHERE unit_price>=300 or unit_price<=400;
-
+# 06
 select author_name from author WHERE country='India' 
 union
 select author_name from author WHERE country='Australia';
 
+# 07
 select * from publisher 
  	WHERE year=2015 and year=2016;
-
+ 	
+# 08(i)
 SELECT * from book ORDER BY unit_price DESC LIMIT 1;
+# or
+select  * from book where unit_price = (select max(unit_price) from book);
 
+# 08(ii)
 select * from book 
 WHERE unit_price between 300 and 400;
-	
+
+# 09	
 SELECT * from book
 	ORDER BY unit_price desc,pub_year desc;
 
+# 10
 select author_no,publisher_no FROM book 
 where pub_year in (2000,2004,2018);
-	
